f3ec8005764b20ca7df0829388a539d25761f2c6
Jon Free <freejonm@gmail.com>
2021-02-19 10:25:43 -0500

    adding server route to index.html

--- server.js
+++ server.js
@@ -11,5 +11,8 @@ const PORT = process.env.PORT || 3000
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Routes
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/Develop/public/index.html')));
+
 // Listener
 app.listen(PORT, () => console.log(`server listening on ${PORT}`))
